f24be90749b7e9ac4537b2b5d0acddb824e51e32
Thiago Martins <thinomar@gmail.com>
2020-12-03 23:58:30 -0300

    Alterações em chamada de comandos

--- src/main.js
+++ src/main.js
@@ -1,60 +1,66 @@
 const { app, BrowserWindow } = require('electron');
 const { spawn } = require('child_process');
 
-async function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
+async function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
 
-  win.loadFile(`${__dirname}/views/index.html`);
+    win.loadFile(`${__dirname}/views/index.html`);
 
-  await runAssessmentTool();
+    await runAssessmentTool();
 }
 
 app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 });
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
 });
 
 async function runAssessmentTool() {
-    const winsat = spawn('powershell', [
-        '-NoProfile', 
-        '-NonInteractive', 
-        '-Command', 
-        '"& {Start-Process WinSAT -ArgumentList \'d3d\' -Verb RunAs -Wait}"'
-    ]);;
+    return new Promise(async (resolve, reject) => {
+        const winsat = spawn('powershell', [
+            '-NoProfile',
+            '-NonInteractive',
+            '-Command',
+            '"& {Start-Process WinSAT -ArgumentList \'d3d\' -Verb RunAs -Wait}"'
+        ]);;
 
-    winsat.stderr.on('data', (data) => console.error(data));
+        winsat.stderr.on('data', (data) => reject(data));
 
-    winsat.on('close', (statusCode) => {
-        getExperienceIndex();
-        console.log(`Returned code: ${statusCode}`);
+        winsat.on('close', async (code) => {
+            console.log(`Returned code: ${code}`);
+            const pcScore = await getExperienceIndex();
+            return resolve(pcScore);
+        });
     });
 }
 
-function getExperienceIndex() {
-    const wei = spawn('powershell.exe', ['-noexit', '"Get-WmiObject -class Win32_WinSAT"']);
+async function getExperienceIndex() {
+    return new Promise((resolve, reject) => {
+        let output = [];
+        const wei = spawn('powershell.exe', ['-noexit', '"Get-WmiObject -class Win32_WinSAT"']);
 
-    wei.stderr.on('data', (data) => console.error(data));
+        wei.stderr.on('data', (data) => reject(data));
 
-    wei.stdout.on('data', (data) => {
-        console.log(data);
-    });
+        wei.stdout.on('data', (data) => output.push(data.toString()));
 
-    wei.on('close', (statusCode) => {
-        console.log(`Returned code: ${statusCode}`);
+        wei.on('close', (code) => {
+            console.log(`Returned code: ${code}`);
+            console.log(output);
+            return resolve(output);
+        });
     });
 }
