34a81956bbf040ef988609cfc17ff471c6d9a7fc
Thiago Martins <thinomar@gmail.com>
2020-12-04 01:34:41 -0300

    Implementação de todas consultas em terminal

--- src/main.js
+++ src/main.js
@@ -31,12 +31,10 @@ app.on('activate', () => {
 
 async function runAssessmentTool() {
     return new Promise(async (resolve, reject) => {
-        const winsat = spawn('powershell', [
-            '-NoProfile',
-            '-NonInteractive',
-            '-Command',
-            '"& {Start-Process WinSAT -ArgumentList \'d3d\' -Verb RunAs -Wait}"'
-        ]);;
+        const winsat = spawn(
+            'Start-Process', 
+            ['WinSAT', '-ArgumentList', 'd3d', '-Verb', 'RunAs', '-Wait'],
+            { shell: 'powershell.exe', windowsHide: true });
 
         winsat.stderr.on('data', (data) => reject(data));
 
@@ -50,17 +48,69 @@ async function runAssessmentTool() {
 
 async function getExperienceIndex() {
     return new Promise((resolve, reject) => {
-        let output = [];
-        const wei = spawn('powershell.exe', ['-noexit', '"Get-WmiObject -class Win32_WinSAT"']);
+        let output = "";
+        const wei = spawn(
+            'Get-WmiObject', 
+            ['-class', 'Win32_WinSAT'], 
+            { shell: 'powershell.exe', windowsHide: true });
 
         wei.stderr.on('data', (data) => reject(data));
 
-        wei.stdout.on('data', (data) => output.push(data.toString()));
+        wei.stdout.on('data', (data) => output += data.toString());
 
-        wei.on('close', (code) => {
-            console.log(`Returned code: ${code}`);
-            console.log(output);
-            return resolve(output);
+        wei.on('close', async (code) => {
+            const experience = await mapWinsatToExperienceIndex(output);
+            return resolve(experience);
         });
     });
+}
+
+async function mapWinsatToExperienceIndex(winsatOutput){    
+    const fields = {
+        'CPUScore': 'cpu',
+        'D3DScore': 'direct3d',
+        'DiskScore': 'disk',
+        'GraphicsScore': 'graphics',
+        'MemoryScore': 'memory',
+        'WinSPRLevel': 'score'
+    };
+
+    const outputLines = winsatOutput.split('\r\n');
+    const properties = outputLines.filter(item => item.split(':')[0].trim() in fields);
+    
+    console.log(properties);
+    
+    const experience = {};
+
+    properties.map(item => {
+        const fieldKey = fields[item.split(':')[0].trim()];
+        const value = item.split(':')[1].trim();
+
+        experience[fieldKey] = parseFloat(value.replace(',','.'));
+    });
+
+    experience['assessmentDate'] = await getAssessmentDate();
+
+    console.log(experience);
+
+    return experience;
+}
+
+async function getAssessmentDate(){
+    return new Promise((resolve, reject) => {
+        let output = "";
+        const wei = spawn(
+            '(gci -recurse c:\\windows\\performance\\winsat\\datastore | select @{name="lastwritetime"; expression={$_.lastwritetime.tostring("yyyy-MM-ddTHH:mmZ")}})[0]', 
+            [], 
+            { shell: 'powershell.exe', windowsHide: true });
+
+        wei.stderr.on('data', (data) => reject(data));
+
+        wei.stdout.on('data', (data) => output += data.toString());
+
+        wei.on('close', (code) => {
+            const date = new Date(output.trim().split('\r\n').pop());
+            return resolve(date);
+        });
+    });    
 }
